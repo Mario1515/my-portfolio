83dbe1b85b05d0a888141912d4f041392bfca9c1
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2024-05-24 21:20:10 +0300

    About, Contact, Experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,15 +10,15 @@ const Hero = () => {
                 <div className='flex flex-col items-center lg:items-start'>
                     <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Mario Petkov</h1>
                     <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text
-                    text-4xl tracking-tight text-transparent'>Full Stack Developer</span>
+                    text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                         {HERO_CONTENT}
                     </p>
                 </div>
             </div>
-            <div className='w-full lg:w-1/2 lg:p-8 lg:mt-[-200px]' >
-                <div className='flex justify-center'>
-                    <img src={profilePic} alt="Mario Petkov" />
+            <div className='w-full lg:w-1/2 lg:p-20 lg:mt-[-250px]' >
+                <div className=' flex justify-center'>
+                    <img  src={profilePic} alt="Mario Petkov" />
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" className="w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64" />
+            <img src={logo} alt="logo" className=" rounded-2xl w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64" />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl' >
             <FaLinkedin />
