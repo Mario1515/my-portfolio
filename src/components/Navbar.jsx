334d225346ffaedc7e9994ad0fd449fe5896bb72
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2024-05-24 22:46:41 +0300

    minor bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaInstagram} from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" className=" rounded-2xl w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64" />
+          <a href="https://your-website.com"> 
+          <img src={logo} alt="logo" className="rounded-2xl w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64" />
+        </a>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl' >
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-        </div>
+        <a href="https://www.linkedin.com/in/mario-petkov-552183206/"> 
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/your-username"> 
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/your-profile"> 
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
   )
 }
